6c3d8d0c0916eb726f5201935c3dbaa29879592d
ycarr5150 <ycarrillo@csumb.edu>
2019-12-03 03:50:36 +0000

    adding files

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express"); 
+const app = express(); 
+
+app.engine('html', require('ejs').renderFile);
+app.use(express.static("public")); 
+
+// routes
+app.get('/', function(req, res, next) {
+    res.render("index.html");
+});
+
+// server listener 
+app.listen(process.env.PORT, process.env.IP, function() {
+    console.log("Running Express Server..."); 
+});
