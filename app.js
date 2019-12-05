8fed58441f7731a8b3af6f56588d8df5a5b2abde
ycarr5150 <ycarrillo@csumb.edu>
2019-12-05 08:29:58 +0000

    updating

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mysql = require('mysql');
 const cookieParser = require('cookie-parser');
+const session = require('express-session');
 const app = express();
 
 app.use(express.json());
@@ -8,6 +9,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use(express.static("public")); 
+
+app.set('trust proxy', 1); // trust first proxy
+app.use(session({
+    secret: 'password'
+})); 
 
 // routes
 app.get('/', function(req, res, next) {
@@ -31,6 +37,24 @@ app.get('/', function(req, res, next) {
     
     connection.end(); 
 });
+
+app.post('/', function(req, res, next) {
+    let successful = false;
+    let message = ""; 
+    
+    if(req.body.idNumber == "") {
+        message = "error, missing number"; 
+    } else {
+        successful = true; 
+        req.session.idNumber = req.body.idNumber; 
+    }
+    
+    // Return success or failure
+    res.json({
+        successful: successful,
+        message: message
+    });
+}); 
 
 app.get('/new-asset', function(req, res, next) {
     res.render("new-asset.hbs");
@@ -86,15 +110,52 @@ app.get('/show-asset', function(req, res, next) {
     
     connection.query(
         `SELECT * FROM assets
-        WHERE id = 3`, function(error, results, fields) {
+        WHERE id = ${req.session.idNumber}`, function(error, results, fields) {
         if (error) throw error;
 
         res.render('show-asset.hbs', {
             item: results
         });
     });
-    
     connection.end(); 
+}); 
+
+app.post('/show-asset', function(req, res, next) {
+    let successful = false;
+    let message = ""; 
+    
+    if(req.body.category == "" || req.body.manufacturer == "" || req.body.model == "" || req.body.serial == "" || req.body.acquired == ""
+        || req.body.paid == "" || req.body.condition == "" || req.body.warrenty == "" || req.body.located == "") {
+        message = "Missing information above. Please fill out to continue"; 
+    } else {
+        // if success, open connection, add asset to database 
+        successful = true; 
+        const connection = mysql.createConnection({
+            host: 'nkpl8b2jg68m87ht.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+            user: 'ns0xtj5joo1dl0j9',
+            password: 'oashevj1wpkaemmg',
+            database: 'h2eeztdsfwtqbwbf'
+        });
+        
+        connection.connect();
+        // connection.query(
+        // `INSERT INTO assets 
+        // (category, manufacturer, model, serial, acquired, price, cond, warrenty, location, retired, description, comments)
+        // VALUES ('${req.body.category}', '${req.body.manufacturer}', '${req.body.model}', '${req.body.serial}', '${req.body.acquired}', 
+        // '${req.body.paid}', '${req.body.condition}', '${req.body.warrenty}', '${req.body.located}', 'N', '${req.body.descrip}', 
+        // '${req.body.comment}')`
+        // , function(error, results, fields) {
+        //     if (error) throw error;
+        // });
+        
+        connection.end(); 
+    }
+    
+    // Return success or failure
+    res.json({
+        successful: successful,
+        message: message
+    });
 }); 
 
 // server listener 
--- public/js/functions.js
+++ public/js/functions.js
@@ -34,3 +34,40 @@ $('#submit-button').on("click", function(e) {
         }
     });
 }); 
+
+$('#update-button').on("click", function(e) {
+    e.preventDefault(); // Do not submit until I am ready
+    
+    $.ajax({
+        type: "POST",
+        url: "/show-asset",
+        dataType: "json",
+        contentType: "application/json",
+        data: JSON.stringify({
+            "category" : $('#category').val(), 
+            "manufacturer" : $('#manufacturer').val(), 
+            "model" : $('#model').val(), 
+            "serial" :$('#serial-number').val(), 
+            "acquired" : $('#acquired-date').val(), 
+            "paid" : $('#purchase-price').val(), 
+            "condition" : $('#condition').val(), 
+            "warrenty" : $('#warrenty-expiration').val(), 
+            "located" : $('#location').val(), 
+            "descrip" : $('#description-box').val(), 
+            "comment" : $('#comment-box').val()
+        }),
+        success: function(result, status) {
+            if (result.successful) {
+                window.location.href = '/'; // This will navigate to wherever i say...
+            }
+            else {
+                // Show an error message or something and stay here
+                $('#message').html(result.message).css("color", "red");
+                $('#message').show();
+            }
+        }, 
+        error: function(xhr, status, message) {
+            console.log("error: ", xhr.responseText); 
+        }
+    });
+}); 
