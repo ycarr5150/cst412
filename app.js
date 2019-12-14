7c858b57c6677c1e278cf6023a7e3e0edf046690
ycarr5150 <ycarrillo@csumb.edu>
2019-12-14 23:14:02 +0000

    udpating code

--- app.js
+++ app.js
@@ -54,15 +54,33 @@ app.get('/new-asset', function(req, res, next) {
 });
 
 app.post('/new-asset', function(req, res, next) {
-    let successful = false;
-    let message = ""; 
-    
-    if(req.body.category == "" || req.body.manufacturer == "" || req.body.model == "" || req.body.serial == "" || req.body.acquired == ""
-        || req.body.paid == "" || req.body.condition == "" || req.body.warrenty == "" || req.body.located == "") {
-        message = "Missing information above. Please fill out to continue"; 
-    } else {
-        // if success, open connection, add asset to database 
-        successful = true; 
+    const connection = mysql.createConnection({
+        host: 'nkpl8b2jg68m87ht.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user: 'ns0xtj5joo1dl0j9',
+        password: 'oashevj1wpkaemmg',
+        database: 'h2eeztdsfwtqbwbf'
+    });
+        
+    connection.connect();
+    connection.query(
+    `INSERT INTO assets 
+    (category, manufacturer, model, serial, acquired, price, cond, warrenty, location, retired, description, comments)
+    VALUES ('${req.body.category}', '${req.body.manufacturer}', '${req.body.model}', '${req.body.serial}', '${req.body.acquired}', 
+    '${req.body.paid}', '${req.body.condition}', '${req.body.warrenty}', '${req.body.located}', 'N', '${req.body.descrip}', 
+    '${req.body.comment}')`
+    , function(error, results, fields) {
+        if (error) throw error;
+        connection.end(); 
+        
+        // Return success or failure
+        res.json({
+            successful: true
+        });
+    });
+});
+
+app.get('/show-asset', function(req, res, next) {
+    if(req.session.idNumber) {
         const connection = mysql.createConnection({
             host: 'nkpl8b2jg68m87ht.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
             user: 'ns0xtj5joo1dl0j9',
@@ -71,56 +89,26 @@ app.post('/new-asset', function(req, res, next) {
         });
         
         connection.connect();
-        connection.query(
-        `INSERT INTO assets 
-        (category, manufacturer, model, serial, acquired, price, cond, warrenty, location, retired, description, comments)
-        VALUES ('${req.body.category}', '${req.body.manufacturer}', '${req.body.model}', '${req.body.serial}', '${req.body.acquired}', 
-        '${req.body.paid}', '${req.body.condition}', '${req.body.warrenty}', '${req.body.located}', 'N', '${req.body.descrip}', 
-        '${req.body.comment}')`
-        , function(error, results, fields) {
-            if (error) throw error;
-        });
         
-        connection.end(); 
-    }
+        connection.query(
+            `SELECT * FROM assets
+            WHERE id = ${req.session.idNumber}`, function(error, results, fields) {
+            if (error) throw error;
+            connection.end(); 
     
-    // Return success or failure
-    res.json({
-        successful: successful,
-        message: message
-    });
-});
-
-app.get('/show-asset', function(req, res, next) {
-    const connection = mysql.createConnection({
-        host: 'nkpl8b2jg68m87ht.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-        user: 'ns0xtj5joo1dl0j9',
-        password: 'oashevj1wpkaemmg',
-        database: 'h2eeztdsfwtqbwbf'
-    });
-    
-    connection.connect();
-    
-    connection.query(
-        `SELECT * FROM assets
-        WHERE id = ${req.session.idNumber}`, function(error, results, fields) {
-        if (error) throw error;
-        connection.end(); 
-
-        res.render('show-asset.hbs', {
-            item: results
+            res.render('show-asset.hbs', {
+                item: results
+            });
         });
-    });
+    } else {
+        res.redirect('/');
+    }
 }); 
 
 app.post('/show-asset', function(req, res, next) {
     let date = new Date();
-    let successful = false;
-    let message = ""; 
     
     if(req.body.retire == "yes") {
-        successful = true; 
-        
         const connection = mysql.createConnection({
             host: 'nkpl8b2jg68m87ht.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
             user: 'ns0xtj5joo1dl0j9',
@@ -134,20 +122,14 @@ app.post('/show-asset', function(req, res, next) {
         `UPDATE assets SET retired = '${date}' WHERE id = '${req.session.idNumber}'`
         , function(error, results, fields) {
             if (error) throw error;
+            connection.end(); 
+            
+            // Return success or failure
+            res.json({
+                successful: true
+            });
         });
-        
-        connection.end(); 
-        
-        // Return success or failure
-        res.json({
-            successful: successful,
-            message: message
-        });
-    }
-    
-    if(req.body.retire == "no") {
-        successful = true; 
-        
+    } else if(req.body.retire == "no") {
         const connection = mysql.createConnection({
             host: 'nkpl8b2jg68m87ht.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
             user: 'ns0xtj5joo1dl0j9',
@@ -161,23 +143,15 @@ app.post('/show-asset', function(req, res, next) {
         `UPDATE assets SET retired = 'N' WHERE id = '${req.session.idNumber}'`
         , function(error, results, fields) {
             if (error) throw error;
+            connection.end(); 
+            
+            // Return success or failure
+            res.json({
+                successful: true
+            });
         });
-        
-        connection.end(); 
-        
-        // Return success or failure
-        res.json({
-            successful: successful,
-            message: message
-        });
-    }
-    
-    if(req.body.category == "" || req.body.manufacturer == "" || req.body.model == "" || req.body.serial == "" || req.body.acquired == ""
-        || req.body.paid == "" || req.body.condition == "" || req.body.warrenty == "" || req.body.located == "") {
-        message = "Missing information above. Please fill out to continue"; 
     } else {
-        // if success, open connection, add asset to database 
-        successful = true; 
+        // if success, open connection, add asset to database
         const connection = mysql.createConnection({
             host: 'nkpl8b2jg68m87ht.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
             user: 'ns0xtj5joo1dl0j9',
@@ -190,20 +164,18 @@ app.post('/show-asset', function(req, res, next) {
         `UPDATE assets
         SET category = '${req.body.category}', manufacturer = '${req.body.manufacturer}', model = '${req.body.model}', serial = '${req.body.serial}', 
         acquired = '${req.body.acquired}', price = '${req.body.paid}', cond = '${req.body.condition}', warrenty = '${req.body.warrenty}', 
-        location = '${req.body.located}', retired = 'N', description = '${req.body.descrip}', comments =  '${req.body.comment}'
+        location = '${req.body.located}', description = '${req.body.descrip}', comments =  '${req.body.comment}'
         WHERE id = ${req.session.idNumber};`
         , function(error, results, fields) {
             if (error) throw error;
+            connection.end(); 
+            
+            // Return success or failure
+            res.json({
+                successful: true 
+            });
         });
-        
-        connection.end(); 
     }
-    
-    // Return success or failure
-    res.json({
-        successful: successful,
-        message: message
-    });
 }); 
 
 // server listener 
